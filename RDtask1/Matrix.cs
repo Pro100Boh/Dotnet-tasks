f49f4d432d0fadc5951b9fb6a12d60bec6b28358
Pro100Boh <veres.bs@gmail.com>
2018-10-07 23:41:19 +0300

    Task 1

---
+++ RDtask1/Matrix.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace RDtask1
+{
+
+    [Serializable]
+    public class MatrixDimensionException : ApplicationException
+    {
+        public MatrixDimensionException() { }
+        public MatrixDimensionException(string message) : base(message) { }
+        public MatrixDimensionException(string message, Exception inner) : base(message, inner) { }
+        protected MatrixDimensionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class MatrixIndexOutOfRangeException : ApplicationException
+    {
+        public MatrixIndexOutOfRangeException() { }
+        public MatrixIndexOutOfRangeException(string message) : base(message) { }
+        public MatrixIndexOutOfRangeException(string message, Exception inner) : base(message, inner) { }
+        protected MatrixIndexOutOfRangeException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+    public class Matrix : ICloneable
+    {
+        private int[,] items;
+
+        public int Rows { get; }
+
+        public int Cols { get; }
+
+        public Matrix(int rows, int cols)
+        {
+            if (rows <= 0 || cols <= 0)
+                throw new MatrixIndexOutOfRangeException($"Unable to create matrix with [{rows}][{cols}] dimensions");
+
+            items = new int[rows, cols];
+
+            Rows = rows;
+            Cols = cols;
+        }
+
+        public Matrix(int[,] items)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            this.items = (int[,])items.Clone();
+
+            Rows = items.GetLength(0);
+            Cols = items.GetLength(1);
+        }
+
+        private bool IsCorrectDimensions(int i, int j) => i >= 0 && i < Rows && j >= 0 && j < Cols;
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (!IsCorrectDimensions(i, j))
+                    throw new MatrixIndexOutOfRangeException($"No such element with indexes [{i}][{j}]");
+
+                return items[i, j];
+            }
+            set
+            {
+                if (!IsCorrectDimensions(i, j))
+                    throw new MatrixIndexOutOfRangeException($"No such element with indexes [{i}][{j}]");
+
+                items[i, j] = value;
+            }
+        }
+
+        private bool IsEqualDimensions(Matrix other) => Rows == other.Rows && Cols == other.Cols;
+
+        public Matrix Add(Matrix b)
+        {
+            if (b == null)
+                throw new ArgumentNullException(nameof(b));
+
+            if (!IsEqualDimensions(b))
+                throw new MatrixDimensionException($"Unable to add matrices with different dimensions [{Rows}][{Cols}], [{b.Rows}][{b.Cols}]");
+
+            Matrix c = (Matrix)Clone();
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    c.items[i, j] += b.items[i, j];
+                }
+            }
+
+            return c;
+        }
+
+        public Matrix Subtract(Matrix b)
+        {
+            if (b == null)
+                throw new ArgumentNullException(nameof(b));
+
+            if (!IsEqualDimensions(b))
+                throw new MatrixDimensionException($"Unable to subtract matrices with different dimensions [{Rows}][{Cols}], [{b.Rows}][{b.Cols}]");
+
+            Matrix c = (Matrix)Clone();
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    c.items[i, j] -= b.items[i, j];
+                }
+            }
+
+            return c;
+        }
+
+        public Matrix Multiply(Matrix b)
+        {
+            if (b == null)
+                throw new ArgumentNullException(nameof(b));
+
+            if (Cols != b.Rows)
+                throw new MatrixDimensionException($"Unable to multiply matrices - j dimension of first matrix ({Cols}) not equals to i dimension of second matrix ({b.Rows})");
+
+            Matrix c = new Matrix(Rows, b.Cols);
+
+            for (int i = 0; i < c.Rows; i++)
+            {
+                for (int j = 0; j < c.Cols; j++)
+                {
+                    int element = 0;
+                    for (int k = 0; k < Cols; k++)
+                    {
+                        element += items[i, k] * b.items[k, j];
+                    }
+                    c.items[i, j] = element;
+                }
+            }
+
+            return c;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Matrix b = obj as Matrix;
+
+            if (obj == null)
+                return false;
+
+            if (Rows != b.Rows || Cols != b.Cols)
+                return false;
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    if (items[i, j] != b.items[i, j])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b) => a.Add(b);
+
+        public static Matrix operator -(Matrix a, Matrix b) => a.Subtract(b);
+
+        public static Matrix operator *(Matrix a, Matrix b) => a.Multiply(b);
+
+        public static bool operator ==(Matrix a, Matrix b) => a.Equals(b);
+
+        public static bool operator !=(Matrix a, Matrix b) => !a.Equals(b);
+
+        public override int GetHashCode() => items.GetHashCode();
+
+        public object Clone() => new Matrix(items);
+
+        public int[,] ToArray() => (int[,])items.Clone();
+    }
+}
---
+++ RDtask1/Polynomial.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace RDtask1
+{
+
+    [Serializable]
+    public class NegativePolymonialExponentException : Exception
+    {
+        public NegativePolymonialExponentException() { }
+        public NegativePolymonialExponentException(string message) : base(message) { }
+        public NegativePolymonialExponentException(string message, Exception inner) : base(message, inner) { }
+        protected NegativePolymonialExponentException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+    public class Polynomial
+    {
+        private double[] coef;
+
+        public int Degree { get; private set; }
+
+        public Polynomial(double leadingСoef, int exponent)
+        {
+            if (exponent < 0)
+                throw new NegativePolymonialExponentException($"Exponent of polynomial can`t be negative: {exponent}");
+            
+            coef = new double[exponent + 1];
+            coef[exponent] = leadingСoef;
+
+            Degree = exponent;
+        }
+
+        public double this[int exponent]
+        {
+            get
+            {
+                if (exponent < 0)
+                    throw new NegativePolymonialExponentException($"Exponent of polynomial can`t be negative: {exponent}");
+
+                if (exponent > Degree)
+                    return 0.0;
+
+                return coef[exponent];
+            }
+            set
+            {
+                if (exponent < 0)
+                    throw new NegativePolymonialExponentException($"Exponent of polynomial can`t be negative: {exponent}");
+
+                if (exponent > Degree)
+                {
+                    var newCoef = new double[coef.Length + 1];
+                    Array.Copy(coef, newCoef, coef.Length);
+
+                    coef = newCoef;
+                    Degree = exponent;
+                }
+
+                coef[exponent] = value;
+                UpdateDegree();
+            }
+        }
+
+        private void UpdateDegree()
+        {
+            Degree = 0;
+            for (int i = coef.Length - 1; i >= 0; i--)
+            {
+                if (coef[i] != 0)
+                {
+                    Degree = i;
+
+                    if (i < coef.Length - 1)
+                    {
+                        int newLength = i + 1;
+                        var newCoef = new double[newLength];
+                        Array.Copy(coef, newCoef, newLength);
+                        coef = newCoef;
+                    }
+                    return;
+                }
+            }
+        }
+
+        public double GetValue(double x)
+        {
+            double result = 0;
+
+            for (int i = Degree; i >= 0; i--)
+                result += coef[i] * Math.Pow(x, i);
+
+            return result;
+        }
+
+        public Polynomial Add(Polynomial other)
+        {
+            if (other == null)
+                throw new ArgumentNullException(nameof(other));
+
+            Polynomial result = new Polynomial(0, Math.Max(Degree, other.Degree));
+
+            for (int i = 0; i <= Degree; i++)
+                result.coef[i] += coef[i];
+
+            for (int i = 0; i <= other.Degree; i++)
+                result.coef[i] += other.coef[i];
+
+            result.UpdateDegree();
+
+            return result;
+        }
+
+        public Polynomial Subtract(Polynomial other)
+        {
+            if (other == null)
+                throw new ArgumentNullException(nameof(other));
+
+            Polynomial result = new Polynomial(0, Math.Max(Degree, other.Degree));
+
+            for (int i = 0; i <= Degree; i++)
+                result.coef[i] += coef[i];
+
+            for (int i = 0; i <= other.Degree; i++)
+                result.coef[i] -= other.coef[i];
+
+            result.UpdateDegree();
+
+            return result;
+        }
+
+        public Polynomial Multiply(Polynomial other)
+        {
+            if (other == null)
+                throw new ArgumentNullException(nameof(other));
+
+            Polynomial result = new Polynomial(0, Degree + other.Degree);
+
+            for (int i = 0; i <= Degree; i++)
+                for (int j = 0; j <= other.Degree; j++)
+                    result.coef[i + j] += (coef[i] * other.coef[j]);
+
+            result.UpdateDegree();
+
+            return result;
+        }
+
+        public static Polynomial operator +(Polynomial a, Polynomial b) => a.Add(b);
+
+        public static Polynomial operator -(Polynomial a, Polynomial b) => a.Subtract(b);
+
+        public static Polynomial operator *(Polynomial a, Polynomial b) => a.Multiply(b);
+
+        public override string ToString()
+        {
+            string result = $"({coef[Degree]}*x^{Degree}) ";
+
+            for (int i = Degree - 1; i >= 0; i--)
+                if (coef[i] != 0)
+                    result += $"+ ({coef[i]}*x^{i}) ";
+
+            return result;
+        }
+    }
+}
