310e008cd4b2faa10385b5840bc3dc8a22ec0424
Pro100Boh <veres.bs@gmail.com>
2018-12-05 18:58:24 +0200

    Rename linq tasks

---
+++ RDtask2/RDtask2.Linq/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RDtask2LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqTask.Task1();
+
+            Console.ReadKey();
+        }
+    }
+}
